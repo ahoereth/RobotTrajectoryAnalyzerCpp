d21d44709f5afda8768da02e21a9e8dbf9c06d8a
Alexander Höreth <a.hoereth@gmail.com>
2015-07-02 15:33:58 +0200

    AccelerationAnnotator functionality.

--- src/AccelerationAnnotator.cpp
+++ src/AccelerationAnnotator.cpp
@@ -96,6 +96,63 @@ class AccelerationAnnotator : public Annotator {
     const uima::ResultSpecification& resultSpecification
   ) {
     log->logMessage("AccelerationAnnotator::process() begins");
+
+    // Initialize features.
+    Feature timeFtr = JointState.getFeatureByBaseName("time");
+    Feature jtpFtr = JointState.getFeatureByBaseName("jointTrajectoryPoint");
+    Feature jtpVelFtr = JointTrajectoryPoint.getFeatureByBaseName("velocities");
+    Feature accVelFtr = Acceleration.getFeatureByBaseName("value");
+
+    // Intialize indices and the index iterator.
+    uima::FSIndexRepository& index = cas.getIndexRepository();
+    uima::ANIndex jsIndex = cas.getAnnotationIndex(JointState);
+    uima::ANIterator jsIter = jsIndex.iterator();
+
+    // Initialize reused variables for the loop.
+    uima::ListFS accelerations = cas.createListFS();
+    uima::FeatureStructure js, jtp;
+    uima::DoubleArrayFS velocitiesPrev, velocitiesNext, values;
+    int timePrev, timeNext;
+    double velocityDiff, timeDiff, accelerationValue;
+
+    if (jsIter.isValid()) {
+      // Get initial values.
+      js = jsIter.get();
+      jtp = js.getFSValue(jtpFtr);
+      timePrev = js.getIntValue(timeFtr);
+      velocitiesPrev = jtp.getDoubleArrayFSValue(jtpVelFtr);
+
+      // Loop through all consecutive joint states.
+      jsIter.moveToNext();
+      while (jsIter.isValid()) {
+        // Get consecutive values.
+        js = jsIter.get();
+        jtp = js.getFSValue(jtpFtr);
+        timeNext = js.getIntValue(timeFtr);
+        velocitiesNext = jtp.getDoubleArrayFSValue(jtpVelFtr);
+        values = cas.createDoubleArrayFS(velocitiesNext.size());
+
+        // Calculate acceleration values.
+        for (std::size_t i = 0; i < values.size(); ++i) {
+          velocityDiff = velocitiesNext.get(i) - velocitiesPrev.get(i);
+          timeDiff = timeNext - timePrev;
+          accelerationValue = (0 != timeDiff ? velocityDiff / timeDiff : 0);
+          values.set(i, accelerationValue);
+        }
+
+        // Create acceleration feature structure and save to annotation index.
+        uima::FeatureStructure acceleration = cas.createFS(Acceleration);
+        acceleration.setFSValue(accVelFtr, values);
+        accelerations.addLast(acceleration);
+        index.addFS(acceleration);
+
+        // Remember values and move to next joint state.
+        velocitiesPrev = jtp.getDoubleArrayFSValue(jtpVelFtr);
+        timePrev = js.getIntValue(timeFtr);
+        jsIter.moveToNext();
+      }
+    }
+
     log->logMessage("AccelerationAnnotator::process() ends");
     return UIMA_ERR_NONE;
   }
